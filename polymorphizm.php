98d5575fadb6795f999b2846b1679d867482156b
DenisPprg <denis.pprg@gmail.com>
2021-03-30 16:57:37 +0300

    Д/З "Полиморфизм"

---
+++ polymorphizm.php
@@ -0,0 +1,101 @@
+<?php
+
+
+class User{
+    protected $name;
+    protected $age;
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setAge(string $age)
+    {
+        $this->age = $age;
+    }
+
+    public function getAge(): string
+    {
+        return $this->age;
+    }
+}
+
+class Worker extends User {
+    private $salary;
+    public function setSalary(string $salary)
+    {
+        $this->salary = $salary;
+    }
+
+    public function getSalary(): string
+    {
+        return $this->salary;
+    }
+}
+
+$ivan = new Worker();
+$ivan->setName('Иван');
+$ivan->setAge(25);
+$ivan->setSalary(1000);
+
+$vasya = new Worker();
+$vasya->setName('Вася');
+$vasya->setAge(26);
+$vasya->setSalary(2000);
+
+$summa = $ivan->getSalary() + $vasya->getSalary();
+echo "<pre>";
+echo 'Сумма зарплат Ивана и Васи-'. $summa;
+
+
+class Student extends User{
+    private $grant;
+    private $course;
+
+    public function setGrant(string $grant)
+    {
+        $this->grant = $grant;
+    }
+
+    public function getGrant(): string
+    {
+        return $this->grant;
+    }
+    public function setCourse(string $course)
+    {
+        $this->course = $course;
+    }
+
+    public function getCourse(): string
+    {
+        return $this->course;
+    }
+}
+
+
+class Driver extends Worker{
+    private $experience;
+    private $category;
+    public function setExpAndCat(string $experience,$category){
+        $this->experience = $experience;
+        $this->category = $category;
+    }
+    public function getExp(): string{
+        return $this->experience;
+    }
+    public function getCat(): string{
+        return $this->category;
+    }
+}
+
+echo "<br />";
+$driver = new Driver();
+$driver->setExpAndCat(5,'A'); //Для проверки
+var_dump($driver);
+
+
+
