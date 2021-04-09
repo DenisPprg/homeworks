1b9ab291554aed159e7864510e2b60f5ea55f859
DenisPprg <denis.pprg@gmail.com>
2021-04-09 17:47:29 +0300

    Д/З "Магические методы"

--- example.php
+++ example.php
@@ -21,23 +21,37 @@ class User{
     {
         return $this->age;
     }
+    public function __destruct()
+    {
+        echo 'Проверка дестркутора' . "\n";
+    }
 }
 
 class Builder extends User {
     private static $counter=0;
-    function __construct($name, $age){
+    public function __construct($name, $age){
         self::$counter++;
+        echo 'Проверка конструктора, имя - '. $name .', возраст - '. $age. "\n";
         $this->name = $name;
         $this->age = $age;
     }
     static function getCount(){
         return self::$counter;
     }
+    public function __toString()
+    {
+        return $this->name . ', ' . $this->age;
+    }
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
 }
 
 $tom = new Builder('Tom', 23);
 $bob = new Builder('Bob', 50);
-$den = new Builder('Den', 26);
 
-echo "Всего пользователей: " .  Builder::getCount();
+
+echo  'Проверка __toString, имя и возраст - '. $bob. "\n";
+echo 'Всего пользователей: ' .  Builder::getCount(). "\n";
 
